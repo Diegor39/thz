c712237322ebcb8ec2cbc4054fbac11f2f6acfb0
diegoor40 <d.rincon@rankup.ai>
2022-01-19 20:50:33 -0600

    d

--- examples/test.js
+++ examples/test.js
@@ -1,39 +1,37 @@
 
 
-document.querySelector('#boton').addEventListener('click',traerDatos());
 
-function traerDatos(){
-    const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', 'test.json', true);
+const xhttp = new XMLHttpRequest();
+xhttp.addEventListener("load", onreadystatechange);
+xhttp.open('GET', 'https://thankz-api.herokuapp.com/api/tokens?pkh=tz1ZLedXnXnPbk43LD1sHHG3NMXG7ZveZ1jr');
+xhttp.send();
 
-    xhttp.send();
-
-    xhttp.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200){
-            let datos = JSON.parse(this.responseText);
-            console.log(datos);
-            let res = document.querySelector('#res');
-            res.innerHTML = '';
+function onreadystatechange(){
+    if (this.readyState == 4 && this.status == 200){
+        let datos = JSON.parse(this.responseText);
+        console.log(datos);
+        let res = document.querySelector('#res');
+        res.innerHTML = '';
             
-            for(let item of datos){
+        for(let item of datos){
                 
                 
-                res.innerHTML += `
+            res.innerHTML += `
 
-                <div tabindex="0" class="card cardho" style="max-width: 13%;" onclick='document.getElementById("resToken").innerHTML = "${item.name}"; document.getElementById("resImg").innerHTML = "https://cloudflare-ipfs.com/ipfs/${item.artifact_uri}"; document.getElementById("toggle").style.display = "block";'>
-                    <img class="card-img-top" src="https://cloudflare-ipfs.com/ipfs/${item.artifact_uri}" alt="Card image cap">
+            <div tabindex="0" class="card cardho" style="max-width: 13%;" onclick='document.getElementById("resToken").innerHTML = "${item.name}"; document.getElementById("resImg").innerHTML = "https://cloudflare-ipfs.com/ipfs/${item.artifact_uri}"; document.getElementById("toggle").style.display = "block";'>
+                <img class="card-img-top" src="https://cloudflare-ipfs.com/ipfs/${item.artifact_uri}" alt="Card image cap">
                     
-                    <div class="card-body">
-                        <p>${item.name}</p>
-                    </div>
-                </div> `
+                <div class="card-body">
+                    <p>${item.name}</p>
+                </div>
+            </div> `
                 
                 
-            }
-            
         }
+            
     }
 }
+
 
 
 
