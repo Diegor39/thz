a9f97752a85d4fa88073c38d57e7f8577c82c405
diegoor40 <d.rincon@rankup.ai>
2022-01-24 08:21:33 -0600

    d

--- examples/test.js
+++ examples/test.js
@@ -1,14 +1,13 @@
 
 
-document.querySelector('#boton').addEventListener('click',traerDatos());
 
-function traerDatos(){
     const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', 'test.json', true);
+    xhttp.addEventListener('load', onreadystatechange)
+    xhttp.open('GET', 'https://thankz-api.herokuapp.com/api/get/tokensByHolder?pkh=tz1UTntWEAGVyNXKRSMsV8RjBwuHEVjHNPdy', true);
 
     xhttp.send();
 
-    xhttp.onreadystatechange = function(){
+    function onreadystatechange() {
         if (this.readyState == 4 && this.status == 200){
             let datos = JSON.parse(this.responseText);
             console.log(datos);
@@ -17,11 +16,14 @@ function traerDatos(){
             
             for(let item of datos){
                 
+                var url_img = item.artifact_uri;
+                url_img = /^ipfs:/;
+                console.log(url_img)
                 
                 res.innerHTML += `
 
                 <div tabindex="0" class="card cardho" style="max-width: 13%;" onclick='document.getElementById("resToken").innerHTML = "${item.name}"; document.getElementById("resImg").innerHTML = "https://cloudflare-ipfs.com/ipfs/${item.artifact_uri}"; document.getElementById("toggle").style.display = "block";'>
-                    <img class="card-img-top" style="max-height: 70%; min-height: 70%;" src="https://cloudflare-ipfs.com/ipfs/${item.artifact_uri}" alt="Card image cap">
+                    <img class="card-img-top" style="max-height: 70%; min-height: 70%;" src="https://cloudflare-ipfs.com/ipfs/${url_img}" alt="Card image cap">
                     
                     <div class="card-body">
                         <p>${item.name}</p>
@@ -33,7 +35,7 @@ function traerDatos(){
             
         }
     }
-}
+
 
 
 
